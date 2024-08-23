25a12586f6ce0a462966cffba5f7e5eaec4ddc01
Francesco-Rapetti <francescorapetti1998@gmail.com>
2024-08-23 18:22:26 +0200

    Clean up code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet, ScrollView, Button, useColorScheme } from 'react-native';
+import { StyleSheet, ScrollView, Button, useColorScheme } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import GroupCard from '@/components/GroupCard';
-import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
-import { Spacer } from '@/components/Spacer';
+import { View } from "@/components/Themed";
+import GroupCard from "@/components/GroupCard";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
+import { Spacer } from "@/components/Spacer";
 
 export default function TabOneScreen() {
   const colorScheme = useColorScheme();
   const { theme } = useMaterial3Theme();
   return (
-    <ScrollView style={[styles.container, {backgroundColor:  theme[colorScheme ?? 'light'].background}]}>
-      {/* <Text style={[styles.title, {color:  theme[colorScheme ?? 'light'].onSurface}]}>Tab One</Text> */}
-      {/* <Button title="Go to Details" onPress={() => {console.log("ciao, bottone di prova")}} /> */}
+    <ScrollView
+      style={[
+        styles.container,
+        { backgroundColor: theme[colorScheme ?? "light"].background },
+      ]}
+    >
       <View style={styles.cardContainer}>
         <GroupCard />
         <GroupCard />
@@ -28,8 +30,6 @@ export default function TabOneScreen() {
         <GroupCard />
       </View>
       <Spacer height={110} />
-      {/* <View style={styles.separator} /> */}
-      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </ScrollView>
   );
 }
@@ -44,18 +44,17 @@ const styles = StyleSheet.create({
   cardContainer: {
     backgroundColor: "transparent",
     flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap', // Aggiunto per far sì che i card passino alla riga successiva se necessario
+    flexDirection: "row",
+    flexWrap: "wrap", // Aggiunto per far sì che i card passino alla riga successiva se necessario
     gap: 8, // Aggiunto per mantenere un po' di spazio tra i card
-    
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
