335abc33ba0545db7bad882c7f40039fe97abd11
Francesco-Rapetti <francescorapetti1998@gmail.com>
2024-08-23 18:23:05 +0200

    Refactor card layout

--- components/GroupCard.tsx
+++ components/GroupCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text, View } from './Themed';
-import { StyleSheet, Dimensions, useColorScheme } from 'react-native';
+import { Text, View } from "./Themed";
+import { StyleSheet, Dimensions, useColorScheme } from "react-native";
 import Colors from "@/constants/Colors";
 import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 
@@ -9,32 +9,46 @@ import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 // }
 
 export default function GroupCard() {
-    const { theme } = useMaterial3Theme();
-    const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+  const colorScheme = useColorScheme();
 
-    return (
-        <View style={[styles.container, {backgroundColor:  theme[colorScheme ?? 'light'].surfaceContainer}, {shadowColor: theme[colorScheme ?? 'light'].shadow}]}>
-            <Text style={[styles.title, {color:  theme[colorScheme ?? 'light'].onSurface}]}>Group Card</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        </View>
-    );
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: theme[colorScheme ?? "light"].surfaceContainer },
+        { shadowColor: theme[colorScheme ?? "light"].shadow },
+      ]}
+    >
+      <Text
+        style={[
+          styles.title,
+          { color: theme[colorScheme ?? "light"].onSurface },
+        ]}
+      >
+        Group Card
+      </Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: (Dimensions.get('window').width / 2) - 12,
-        aspectRatio: 3/4,
-        alignSelf: "flex-start",
-        borderRadius: 16,
-        padding: 8,
-        shadowOffset: { width: 0, height: 3 },
-        // shadowRadius: 10,
-        shadowOpacity: 0.1,
-    },
-    title: {
-
-    },
-    separator: {
-
-    },
-})
+  container: {
+    width: Dimensions.get("window").width / 2 - 12,
+    aspectRatio: 3 / 4,
+    alignSelf: "flex-start",
+    borderRadius: 16,
+    padding: 8,
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {},
+  separator: {},
+});
