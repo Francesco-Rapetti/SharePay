917b14b36f075cd988c9335e38637f9ed4d7e4a8
Francesco-Rapetti <francescorapetti1998@gmail.com>
2024-08-26 11:07:48 +0200

    Format file

--- components/PaletteCard.tsx
+++ components/PaletteCard.tsx
@@ -1,40 +1,57 @@
 import React from "react";
-import { Text, View } from './Themed';
-import { StyleSheet, Dimensions, useColorScheme } from 'react-native';
+import { Text, View } from "./Themed";
+import { StyleSheet, useColorScheme } from "react-native";
 import Colors from "@/constants/Colors";
 import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 
 type PaletteCardProps = {
-    materialColor: string;
-    style?: any;
-    fontColor?: any;
-}
+  materialColor: string;
+  style?: any;
+  fontColor?: any;
+};
 
-export default function PaletteCard({ materialColor, style, fontColor }: PaletteCardProps) {
-    const colorScheme = useColorScheme();
+export default function PaletteCard({
+  materialColor,
+  style,
+  fontColor,
+}: PaletteCardProps) {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
 
-    const { theme } = useMaterial3Theme();
-    return (
-        <View style={[styles.container, {backgroundColor:  theme[colorScheme ?? 'light'].surfaceContainerHigh}, style]}>
-            <Text style={[styles.title, {color:  fontColor ?? theme[colorScheme ?? 'light'].onSurface}]}>{materialColor}</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        </View>
-    );
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: theme[colorScheme ?? "light"].surfaceContainerHigh },
+        style,
+      ]}
+    >
+      <Text
+        style={[
+          styles.title,
+          { color: fontColor ?? theme[colorScheme ?? "light"].onSurface },
+        ]}
+      >
+        {materialColor}
+      </Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        height: 100,
-        alignSelf: "center",
-        borderRadius: 16,
-        padding: 8,
-        marginBottom: 8,
-    },
-    title: {
-
-    },
-    separator: {
-
-    },
-})
+  container: {
+    width: "100%",
+    height: 100,
+    alignSelf: "center",
+    borderRadius: 16,
+    padding: 8,
+    marginBottom: 8,
+  },
+  title: {},
+  separator: {},
+});
